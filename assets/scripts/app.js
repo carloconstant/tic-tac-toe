3f948f943784713f6bcf91482e08c96846c54f00
Carlo Constant <carloconstant09@aol.com>
2021-02-16 09:17:49 -0500

    Carlo Constant

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,20 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const api = require('./../scripts/auth/api')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const authEvents = require('./auth/functions')
 $(() => {
   // your JS code goes here
+  $('#sign-up').on('submit',authEvents.onSignUp)
+  $('#sign-in').on('submit',authEvents.onSignIn)
+  $('#change-password').on('submit',authEvents.on)
+  //$('#').on('submit',authEvents.on
+  // $('#change-password').on('submit',authEvents.onChangePassword)
+  // $('#sign-out').on('submit',authEvents.onSignOut)
+  // $('#sign-in').on('submit',authEvents.onSignIn)
+
+//  $('')
 })
